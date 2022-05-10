a629dcb29c79ef63de4b0092f5e5abdd089c0a91
Jawad63 <jawadqureshi111@gmail.com>
2022-05-10 20:30:11 +0200

    files added

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+   autoScrolling: true,
+   navigation: true,
+   anchors: ['section-one', 'section-two', 'section-three'],
+   navigationTooltips: ['Home', 'About us', 'Portfolio', 'Other Projects', 'Contact'],
+   showActiveTooltip: true,
+   scrollingSpeed: 1200,
+   controlArrows: false,
+   slidesNavigation: true,
+});
