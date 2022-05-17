f34dd9c4297543cefc30f8d119863cc9c6b00a1c
Jawad63 <jawadqureshi111@gmail.com>
2022-05-17 03:41:50 +0200

    homepage and navigation is done

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,25 @@
 new fullpage('#fullpage', {
    autoScrolling: true,
    navigation: true,
-   anchors: ['section-one', 'section-two', 'section-three'],
-   navigationTooltips: ['Home', 'About us', 'Portfolio', 'Other Projects', 'Contact'],
+   anchors:
+   [
+      'section-one',
+      'section-two',
+      'section-three',
+      'section-four',
+   ],
+   navigationTooltips: ['Home', 'Work', 'About us', 'Contact'],
    showActiveTooltip: true,
    scrollingSpeed: 1200,
    controlArrows: false,
    slidesNavigation: true,
 });
+
+
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+toggle.addEventListener('click', () => {
+   toggle.classList.toggle('active');
+   navigation.classList.toggle('active');
+})
