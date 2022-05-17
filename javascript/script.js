29ebd9ef67b926ce11a0498561d7e4f126334d70
Jawad63 <jawadqureshi111@gmail.com>
2022-05-17 22:06:58 +0200

    worked with faruk on some ideas

--- javascript/script.js
+++ javascript/script.js
@@ -23,3 +23,37 @@ toggle.addEventListener('click', () => {
    toggle.classList.toggle('active');
    navigation.classList.toggle('active');
 })
+
+
+
+const imageOne = document.getElementById('image-1'); 
+const imageTwo = document.getElementById('image-2');
+const imageThree = document.getElementById('image-3');
+
+
+function mouseHoverNone() {
+   imageOne.style.display = "none";
+   imageTwo.style.display = "none";
+   imageThree.style.display = "none";
+}
+
+
+function mouseHoverOne() {
+   imageOne.style.display = "block";
+   imageTwo.style.display = "none";
+   imageThree.style.display = "none";
+}
+    
+function mouseHoverTwo() {
+   imageOne.style.display = "none";
+   imageTwo.style.display = "block";
+   imageThree.style.display = "none";
+}
+
+function mouseHoverThree() {
+   imageOne.style.display = "none";
+   imageTwo.style.display = "none";
+   imageThree.style.display = "block";
+}
+
+
