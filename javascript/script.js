269fabe521f06b43c2f3b0c81d5852933750cb70
HakiHamza <haktan.boga@hotmail.com>
2022-06-03 18:44:44 +0200

    responsive testing pillars and slides

--- javascript/script.js
+++ javascript/script.js
@@ -1,62 +1,51 @@
 // Fullpage.js sections and slides configuration
 
-new fullpage('#fullpage', {
-   autoScrolling: true,
-   navigation: true,
-   anchors:
-   [
-      'section-one',
-      'section-two',
-      'section-three',
-      'section-four',
-   ],
-   navigationTooltips: ['Home', 'Work', 'About us', 'Contact'],
-   showActiveTooltip: true,
-   scrollingSpeed: 1200,
-   controlArrows: false,
-   slidesNavigation: true,
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  navigation: true,
+  anchors: ["section-one", "section-two", "section-three", "section-four"],
+  navigationTooltips: ["Home", "Work", "About us", "Contact"],
+  showActiveTooltip: true,
+  scrollingSpeed: 1200,
+  controlArrows: false,
+  slidesNavigation: true,
 });
-
 
 // Toggle menu
 
-const toggle = document.querySelector('.toggle');
-const navigation = document.querySelector('.navigation');
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
 
-toggle.addEventListener('click', () => {
-   toggle.classList.toggle('active');
-   navigation.classList.toggle('active');
-})
-
-
-
-
-
-// About Us effect: 
-
-const aboutUsOne = document.getElementById('button-one');
-const aboutUsTwo = document.getElementById('button-two');
-const aboutUsThree = document.getElementById('button-three');
-const aboutUsFour = document.getElementById('button-four');
-const aboutUsImage = document.getElementById('image');
-const aboutUsText = document.getElementById('text');
-
-aboutUsOne.addEventListener('click', () => {
-   aboutUsText.innerText = 'This text is about Consultancy.';
-   aboutUsImage.src = './images/image-one.jpg';
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
 });
 
-aboutUsTwo.addEventListener('click', () => {
-   aboutUsText.innerText = 'This text is about Digital-Marketing.';
-   aboutUsImage.src = './images/image-two.jpg';
+// About Us effect:
+
+const aboutUsOne = document.getElementById("button-one");
+const aboutUsTwo = document.getElementById("button-two");
+const aboutUsThree = document.getElementById("button-three");
+const aboutUsFour = document.getElementById("button-four");
+const aboutUsImage = document.getElementById("image");
+const aboutUsText = document.getElementById("text");
+
+aboutUsOne.addEventListener("click", () => {
+  aboutUsText.innerText = "This text is about Consultancy.";
+  aboutUsImage.src = "./images/image-one.jpg";
 });
 
-aboutUsThree.addEventListener('click', () => {
-   aboutUsText.innerText = 'This text is about Construction-Advice.';
-   aboutUsImage.src = './images/image-three.jpg';
+aboutUsTwo.addEventListener("click", () => {
+  aboutUsText.innerText = "This text is about Digital-Marketing.";
+  aboutUsImage.src = "./images/image-two.jpg";
 });
 
-aboutUsFour.addEventListener('click', () => {
-   aboutUsText.innerText = 'This text is about Web-Development.';
-   aboutUsImage.src = './images/image-four.jpg';
+aboutUsThree.addEventListener("click", () => {
+  aboutUsText.innerText = "This text is about Construction-Advice.";
+  aboutUsImage.src = "./images/image-three.jpg";
+});
+
+aboutUsFour.addEventListener("click", () => {
+  aboutUsText.innerText = "This text is about Web-Development.";
+  aboutUsImage.src = "./images/image-four.jpg";
 });
