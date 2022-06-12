1474c6dcf4128c0b71c2bac6044609fc8868a78d
Jawad63 <jawadqureshi111@gmail.com>
2022-06-12 16:41:50 +0200

    about us section complete and image quality changed

--- javascript/script.js
+++ javascript/script.js
@@ -31,24 +31,83 @@ const aboutUsImage = document.getElementById("image");
 const aboutUsText = document.getElementById("text");
 
 aboutUsOne.addEventListener("click", () => {
-  aboutUsText.innerText = "This text is about Consultancy.";
   aboutUsImage.src = "./images/body-images/slide-one.jpg";
+  aboutUsText.innerText = 
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+    " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" + 
+ 
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+    " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
+
+    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+
+    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+    " maiores, illum quaerat omnis quod, inventore odit rerum. \n "
+  ;
 });
 
 aboutUsTwo.addEventListener("click", () => {
-  aboutUsText.innerText = "This text is about Digital-Marketing.";
   aboutUsImage.src = "./images/body-images/slide-two.jpg";
+  aboutUsText.innerText = 
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. sed mollitia in sint \n" +  
+    " doloribus eaque enim eos? Accusamus sit, ducimus ipsum asperiores \n " +
+    " temporibus amet. Culpa, iusto. Lorem, ipsum dolor sit amet consectetur adipisicing elit. \n " +
+    " Cupiditate placeat ad sed. sit amet consectetur \n" + 
+ 
+    "  rem, in rerum illum! Laborum iste perferendis quos voluptatum ipsam \n " +
+    "  maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
+
+    " \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt explicabo itaque nihil \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+
+    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " deserunt necessitatibus facere repellat, maiores, quidem veniam quibusdam pariatur? \n " +
+    " Cum a nulla aliquam magni \n "
+  ;
 });
 
 aboutUsThree.addEventListener("click", () => {
-  aboutUsText.innerText = "This text is about Construction-Advice.";
   aboutUsImage.src = "./images/body-images/slide-three.jpg";
+  aboutUsText.innerText = 
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+    " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" + 
+ 
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+    " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
+
+    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+
+    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+    " maiores, illum quaerat omnis quod, inventore odit rerum. \n "
+  ;
 });
 
 aboutUsFour.addEventListener("click", () => {
-  aboutUsText.innerText = "This text is about Web-Development.";
   aboutUsImage.src = "./images/body-images/slide-four.jpg";
+  aboutUsText.innerText = 
+  " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sed mollitia in sint \n" +  
+  " doloribus eaque enim eos? Accusamus sit, ducimus ipsum asperiores saepe, porro obcaecati \n " +
+  " temporibus amet. Culpa, iusto. \n " +
+  " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed. \n" + 
+
+  "  rem, in rerum illum! Laborum iste perferendis quos voluptatum ipsam \n " +
+  "  maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
+
+  " \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt explicabo itaque nihil \n" +  
+  " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
+
+  " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+  " deserunt necessitatibus facere repellat, maiores, quidem veniam quibusdam pariatur? \n " +
+  " Cum a nulla aliquam magni \n "
+  ;
+
+
 });
-
-
-console.log(aboutUsImage);
