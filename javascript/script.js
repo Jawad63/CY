43f6252c0dd62ccbcaa311fdca8c1084803f42c9
HakiHamza <haktan.boga@hotmail.com>
2022-06-14 12:56:02 +0200

    cy repsonsive

--- javascript/script.js
+++ javascript/script.js
@@ -5,14 +5,12 @@ new fullpage("#fullpage", {
   navigation: true,
   anchors: ["section-one", "section-two", "section-three", "section-four"],
   navigationTooltips: ["Home", "Work", "About us", "Contact"],
-  horizontalNavigationToolTip: ['tooltip1','tooltip2'],
+  horizontalNavigationToolTip: ["tooltip1", "tooltip2"],
   showActiveTooltip: true,
   scrollingSpeed: 1200,
   controlArrows: false,
   slidesNavigation: true,
 });
-
-
 
 // Toggle menu
 
@@ -35,83 +33,44 @@ const aboutUsText = document.getElementById("text");
 
 aboutUsOne.addEventListener("click", () => {
   aboutUsImage.src = "./images/body-images/slide-one.jpg";
-  aboutUsText.innerText = 
-    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+  aboutUsText.innerText =
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +
     " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
     " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
-    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" + 
- 
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-    " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
-
-    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-
-    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-    " maiores, illum quaerat omnis quod, inventore odit rerum. \n "
-  ;
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n";
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
 });
 
 aboutUsTwo.addEventListener("click", () => {
   aboutUsImage.src = "./images/body-images/slide-two.jpg";
-  aboutUsText.innerText = 
-    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. sed mollitia in sint \n" +  
+  aboutUsText.innerText =
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. sed mollitia in sint \n" +
     " doloribus eaque enim eos? Accusamus sit, ducimus ipsum asperiores \n " +
     " temporibus amet. Culpa, iusto. Lorem, ipsum dolor sit amet consectetur adipisicing elit. \n " +
-    " Cupiditate placeat ad sed. sit amet consectetur \n" + 
- 
-    "  rem, in rerum illum! Laborum iste perferendis quos voluptatum ipsam \n " +
-    "  maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
-
-    " \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt explicabo itaque nihil \n" +  
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-
-    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
-    " deserunt necessitatibus facere repellat, maiores, quidem veniam quibusdam pariatur? \n " +
-    " Cum a nulla aliquam magni \n "
-  ;
+    " Cupiditate placeat ad sed. sit amet consectetur \n";
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
 });
 
 aboutUsThree.addEventListener("click", () => {
   aboutUsImage.src = "./images/body-images/slide-three.jpg";
-  aboutUsText.innerText = 
-    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
+  aboutUsText.innerText =
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +
     " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
     " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
-    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" + 
- 
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-    " maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
-
-    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-
-    " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
-    " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-    " maiores, illum quaerat omnis quod, inventore odit rerum. \n "
-  ;
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n";
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
 });
 
 aboutUsFour.addEventListener("click", () => {
   aboutUsImage.src = "./images/body-images/slide-four.jpg";
-  aboutUsText.innerText = 
-  " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sed mollitia in sint \n" +  
-  " doloribus eaque enim eos? Accusamus sit, ducimus ipsum asperiores saepe, porro obcaecati \n " +
-  " temporibus amet. Culpa, iusto. \n " +
-  " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed. \n" + 
-
-  "  rem, in rerum illum! Laborum iste perferendis quos voluptatum ipsam \n " +
-  "  maiores, illum quaerat omnis quod, inventore odit rerum. \n " +
-
-  " \n Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt explicabo itaque nihil \n" +  
-  " tempora minima reiciendis vero similique dolores quisquam itaque dolorem sit \n " +
-
-  " \n Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n" +  
-  " deserunt necessitatibus facere repellat, maiores, quidem veniam quibusdam pariatur? \n " +
-  " Cum a nulla aliquam magni \n "
-  ;
-
-
+  aboutUsText.innerText =
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum sed mollitia in sint \n" +
+    " doloribus eaque enim eos? Accusamus sit, ducimus ipsum asperiores saepe, porro obcaecati \n " +
+    " temporibus amet. Culpa, iusto. \n " +
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed. \n";
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
+  (" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate placeat ad sed \n");
 });
-
